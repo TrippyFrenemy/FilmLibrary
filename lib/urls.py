34f2393a012b680f5e0f066ddd4f379ee3728755
Avruschenko Dmytro <trippyfren@gmail.com>
2023-05-08 18:49:57 +0300

    Create search panel

--- lib/urls.py
+++ lib/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("category/<slug:category_slug>",  views.home, name="films_by_category"),
     path("category/<slug:category_slug>/<slug:film_slug>",  views.film_detail, name="film_detail"),
+    path("search/",  views.search, name="search"),
     path("about/", views.about, name="about"),
 ]
