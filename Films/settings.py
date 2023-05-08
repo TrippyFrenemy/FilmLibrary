cbb2e588beb398b66b69b3c3cf5efe58d0831cb1
Avruschenko Dmytro <trippyfren@gmail.com>
2023-05-08 17:33:56 +0300

    Change settings files for heroku

--- Films/settings.py
+++ Films/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,12 +143,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
+django_heroku.settings(locals())
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
